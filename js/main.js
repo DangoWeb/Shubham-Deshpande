f8bcccb63a2848477342bfd918b97c5a48a9ee43
goboprogrammer <68043643+goboprogrammer@users.noreply.github.com>
2021-07-25 15:10:08 +0530

    google sign in

--- js/main.js
+++ js/main.js
@@ -26,3 +26,11 @@ var x = setInterval(function() {
     document.getElementById("demo").innerHTML = "EXPIRED";
   }
 }, 1000);
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
