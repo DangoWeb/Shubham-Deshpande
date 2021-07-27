7d6e33e61eeb59aa28129fc66ac020cbcf9d5d66
goboprogrammer <68043643+goboprogrammer@users.noreply.github.com>
2021-07-27 13:16:34 +0530

    test

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<form action="mailto:shubham30052007@gmail.com"
+
+
+method="POST"
+
+
+enctype="multipart/form-data"
+
+
+name="EmailForm">
+
+
+Name:<br>
+
+
+<input type=”text” size="19" name="Shubham"><br><br>
+
+
+Message:<br> <textarea name=”ContactCommentt” rows=”6″ cols=”20″>
+
+
+</textarea><br><br> <input type=”submit” value=”Submit”> </form>
